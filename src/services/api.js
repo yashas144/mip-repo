f4a54fb2a4634ce1264f8b1a99e4dcb55235771d
yashas144 <yashas144@gmail.com>
2026-04-04 23:45:45 -0500

    Initial commit 4

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+const BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8000";
 
 export async function sendChatMessage(message) {
   const response = await fetch(`${BASE_URL}/chat`, {
